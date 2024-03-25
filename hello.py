2ccfe3a68d5a5e0d18baaf6495bad9545621abca
JWellman4 <52516150+JWellman4@users.noreply.github.com>
2024-03-25 19:23:27 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,27 @@
 import streamlit as st
 import pandas as pd
-import plotly as px
+import plotly_express as px
 
-hello = "Hello World"
+st.set_page_config(
+    page_title="Office vs Remote",
+    page_icon="bar_chart",
+    layout="wide",
+)
 
-st.write(hello)
+@st.cache_data
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+# Load the data
+df = load_data('Correlation.csv')
+
+fig =px.scatter(
+        df, 
+        x='TotalHoursAtWork (Sum)',
+        y='TotalHoursWorkOut (Sum)', 
+        color='Work Location',
+        hover_data= 'Date',
+        labels={'Work Location': 'Location','Date':'Week Ending','TotalHoursAtWork (Sum)': 'Hours At Work', 
+                'TotalHoursWorkOut (Sum)': 'Output Hours'} 
+    )
+st.plotly_chart(fig)
